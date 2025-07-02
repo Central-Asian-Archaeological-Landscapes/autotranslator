352ae40437646a8999b987a21f566cd9303fcb76
Mahmoud Abdelrazek <razekmh@gmail.com>
2025-07-02 11:43:59 +0100

    Refactor: fix gloss location
    - Remove unused imports
    - format fixes
    - use Pathlib instead of os for files
    - put correct location for gloss

--- main.py
+++ main.py
@@ -1,52 +1,111 @@
-'''
+"""
 Created on 19 Jun 2022
 
 @author: turch
-'''
-import threading
+"""
+
 import argparse
-import os
 from set_parameters import Begin
-import argparse
+import pathlib
+
 
 def init():
-    parser = argparse.ArgumentParser(description='Automatic translation module with glossary support')
-    gloss = 'glossary.xlsx'
-    project_folder = os.path.split(os.path.dirname(os.path.abspath(__file__)))[0]
-       
-    parser.add_argument('-d', '--data_path', type=str, required=True, help='Path to folder/file with data to translate')
-    parser.add_argument('-i', '--ilanguage', type=str, choices=['ru', 'en'], default='ru', help="Language of the input data (options: 'ru', 'en')")
-    parser.add_argument('-l', '--olanguage', type=str, default='en', help='Language to translate the data into')
-    parser.add_argument('-g', '--glossfile', type=str, default='glossary.xlsx', help='Path to the glossary file (e.g., glossary.xlsx)')
+    parser = argparse.ArgumentParser(
+        description="Automatic translation module with glossary support"
+    )
+    current_file_location = pathlib.Path(__file__).parent.resolve()
+    gloss_path = pathlib.Path(current_file_location / "glossary.xlsx")
 
-    parser.add_argument('-s', '--sheet', type=str, choices=['Monuments', 'Archive', 'Other'], default ='Archive',required=True, help='Sheet name to translate')
-    parser.add_argument('-c','--input', nargs ='+', type=str, required=True, help='The columns to be translated')
-    parser.add_argument('-o', '--output', nargs='+', type=str, required=True, help='The columns to insert translated data into. Please type in corresponding order to input columns')
-    parser.add_argument('-r', '--start_row', type=int, default=5, help='Row to start translation from (excluding column names, default: 5)')
+    parser.add_argument(
+        "-d",
+        "--data_path",
+        type=str,
+        required=True,
+        help="Path to folder/file with data to translate",
+    )
+    parser.add_argument(
+        "-i",
+        "--ilanguage",
+        type=str,
+        choices=["ru", "en"],
+        default="ru",
+        help="Language of the input data (options: 'ru', 'en')",
+    )
+    parser.add_argument(
+        "-l",
+        "--olanguage",
+        type=str,
+        default="en",
+        help="Language to translate the data into",
+    )
+    parser.add_argument(
+        "-g",
+        "--glossfile",
+        type=str,
+        default="glossary.xlsx",
+        help="Path to the glossary file (e.g., glossary.xlsx)",
+    )
+
+    parser.add_argument(
+        "-s",
+        "--sheet",
+        type=str,
+        choices=["Monuments", "Archive", "Other"],
+        default="Archive",
+        required=True,
+        help="Sheet name to translate",
+    )
+    parser.add_argument(
+        "-c",
+        "--input",
+        nargs="+",
+        type=str,
+        required=True,
+        help="The columns to be translated",
+    )
+    parser.add_argument(
+        "-o",
+        "--output",
+        nargs="+",
+        type=str,
+        required=True,
+        help="The columns to insert translated data into. Please type in corresponding order to input columns",
+    )
+    parser.add_argument(
+        "-r",
+        "--start_row",
+        type=int,
+        default=5,
+        help="Row to start translation from (excluding column names, default: 5)",
+    )
     args = parser.parse_args()
-        
-    if args.sheet == 'Monuments':
-        sheet = 'Data Sheet'
-    elif args.sheet == 'Archive':
-        sheet = '2.Описание'
-    else:
-        sheet = input('Please enter name of the sheet to translate: ')
 
-    gloss_path = os.path.join(os.path.dirname(project_folder), args.glossfile)
-    if not os.path.exists(gloss_path):
-        raise FileNotFoundError(f"Glossary file not found at {gloss_path}. Please provide a valid path.")
-    filetype = 'Excel'  # fixed as per original function
+    if args.sheet == "Monuments":
+        sheet = "Data Sheet"
+    elif args.sheet == "Archive":
+        sheet = "2.Описание"
+    else:
+        sheet = input("Please enter name of the sheet to translate: ")
+
+    if not gloss_path.exists():
+        raise FileNotFoundError(
+            f"Glossary file not found at {gloss_path}. Please provide a valid path."
+        )
+    filetype = "Excel"  # fixed as per original function
     print(gloss_path)
-#
-    Begin.modrun(self=None,   
-                 data_path=args.data_path,
-                 ilanguage=args.ilanguage,
-                 olanguage=args.olanguage,
-                 filetype=filetype,
-                 glossfile=args.glossfile, 
-                 sheet=sheet, 
-                 input_column=args.input, 
-                 output_column=args.output, 
-                 start_row=args.start_row)
-    
+    #
+    Begin.modrun(
+        self=None,
+        data_path=args.data_path,
+        ilanguage=args.ilanguage,
+        olanguage=args.olanguage,
+        filetype=filetype,
+        glossfile=args.glossfile,
+        sheet=sheet,
+        input_column=args.input,
+        output_column=args.output,
+        start_row=args.start_row,
+    )
+
+
 init()
